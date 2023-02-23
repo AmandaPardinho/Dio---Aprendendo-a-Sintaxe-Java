f346d33b0b2c738d863ecea176d428cfb0108e95
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-22 22:44:56 -0300

    First code using java has been created

--- anatomia-de-classes-aula1/src/anatomiajava/MinhaClasse.java
+++ anatomia-de-classes-aula1/src/anatomiajava/MinhaClasse.java
@@ -1,2 +1,7 @@
-package anatomiajava;public class MinhaClasse {
+package anatomiajava;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo! Primeiro programa Java desse novo módulo!");
+    }
 }
