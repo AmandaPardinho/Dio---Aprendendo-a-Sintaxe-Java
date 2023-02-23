125c42c87d9b296ff3116137ceee84dd0b7a2f53
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-23 00:58:19 -0300

    Explanation of constants in java

--- tipos-e-variaveis/src/tiposevariaveis/Contantes.java
+++ tipos-e-variaveis/src/tiposevariaveis/Contantes.java
@@ -1,2 +1,21 @@
-package tiposevariaveis;public class Contantes {
+package tiposevariaveis;
+
+public class Contantes {
+    public static void main(String[] args) {
+        //Quando queremos declarar uma variável cujo valor não pode sofrer alteração durante o tempo de execução do
+        // código, usa-se a palavra final
+            //isso criará uma constante e seu valor não poderá ser alterado
+                //essa constante pode ser chamada também de variável final => passa-se a considerar constante aquela
+                    //que é declarada como public static final tipoDaVariavel nomeDaVariavel (divergências encontradas
+                    //(????))
+            //o nome da constante será sempre em caixa alta
+            //erro apresentado (antes de comentar a linha 13): java: cannot assign a value to final variable VALOR
+
+         final float VALOR = 3.14f;
+
+        //VALOR = 5.6f;
+
+        System.out.println(VALOR);
+
+    }
 }
