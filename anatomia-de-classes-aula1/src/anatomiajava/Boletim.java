952c581eec9d36daf715773eb7b1304edcc64745
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-22 23:50:13 -0300

    Created note checking algorithm to illustrate indentation

--- anatomia-de-classes-aula1/src/anatomiajava/Boletim.java
+++ anatomia-de-classes-aula1/src/anatomiajava/Boletim.java
@@ -1,2 +1,15 @@
-package anatomiajava;public class Boletim {
+package anatomiajava;
+
+public class Boletim {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+
+        if(mediaFinal < 6){
+            System.out.println("Reprovado");
+        } else if(mediaFinal == 6){
+            System.out.println("Prova Minerva");
+        } else {
+            System.out.println("Aprovado");
+        }
+    }
 }
