89149f67f5d6815bd743ca31fbb5a9355a713d2e
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-22 23:38:03 -0300

    Code has been created - using concat method

--- anatomia-de-classes-aula1/src/anatomiajava/VariaveisEAtribuicoes.java
+++ anatomia-de-classes-aula1/src/anatomiajava/VariaveisEAtribuicoes.java
@@ -1,2 +1,15 @@
-package anatomiajava;public class VariaveisEAtribuicoes {
+package anatomiajava;
+
+public class VariaveisEAtribuicoes {
+    public static void main(String[] args) {
+        String nome = "Amanda";
+        String sobrenome = "Pardinho";
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String nome, String sobrenome){
+        return nome.concat(" ").concat(sobrenome);
+    }
 }
