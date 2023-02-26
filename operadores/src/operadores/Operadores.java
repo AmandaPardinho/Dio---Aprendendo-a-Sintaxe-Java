27c5b664f309b88f372e1685a1308d67a7c19115
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-26 20:36:54 -0300

    unary operators - increment and decrement

--- operadores/src/operadores/Operadores.java
+++ operadores/src/operadores/Operadores.java
@@ -2,10 +2,12 @@ package operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
+        int numero1 = 5;
 
-        System.out.println(-numero);
-        System.out.println(numero);
-        System.out.println(++numero);
+        System.out.println(-numero1);
+        System.out.println(numero1);
+        System.out.println(++numero1);
+        int numero2 = 4;
+        System.out.println(--numero2);
     }
 }
