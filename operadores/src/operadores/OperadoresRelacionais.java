3aec15838490e7287ba3756c8ddc6b9e84af1274
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-26 22:40:59 -0300

    Code started with comparing integers

--- operadores/src/operadores/OperadoresRelacionais.java
+++ operadores/src/operadores/OperadoresRelacionais.java
@@ -1,2 +1,11 @@
-package operadores;public class OperadoresRelacionais {
+package operadores;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println(simNao);
+    }
 }
