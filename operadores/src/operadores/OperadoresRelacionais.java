8b413e495a4b6ba4027d3f2e9720e63c1171d821
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-26 23:02:41 -0300

    Comparing objects

--- operadores/src/operadores/OperadoresRelacionais.java
+++ operadores/src/operadores/OperadoresRelacionais.java
@@ -14,5 +14,15 @@ public class OperadoresRelacionais {
         String nome2 = "Amanda";
 
         System.out.println(nome1 == nome2);
+
+        //comparando objetos => cada objeto ocupará um espaço diferente na memória => return = false
+            //quando se compara objetos se usa o método .equals() e, se ambos tiverem o mesmo conteúdo interno =>
+        //return true
+
+        String nome3 = "Amanda";
+        String nome4 = new String("Amanda");
+
+        System.out.println(nome3 == nome4);
+        System.out.println(nome3.equals(nome4));
     }
 }
