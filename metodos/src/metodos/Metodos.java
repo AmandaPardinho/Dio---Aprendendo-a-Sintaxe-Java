d6a1a5c0e446e598ed1b8d225c8bd24c4b4e07be
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-26 23:55:48 -0300

    Method Explanations

--- metodos/src/metodos/Metodos.java
+++ metodos/src/metodos/Metodos.java
@@ -2,6 +2,20 @@ package metodos;
 
 public class Metodos {
     public static void main(String[] args) {
-        
+        /* métodos => são todas as ações de uma aplicação
+            -nomeação de métodos:
+                -deve ser nomeado como verbo
+                -padrão camelCase
+            -em java não existe o conceito de métodos globais => todos os métodos devem ser sempre definidos dentro
+            de uma classe
+            -critérios de definição de métodos:
+                -qual a proposta principal do método?
+                -qual o tipo de retorno esperado? => observar se o método tem ou não retorno
+                    -caso o método não retorne nenhum valor, ele será representado pela palavra 'void'
+                -quais os parâmetros necessários para executar o método?
+                -há algum risco de o método apresentar alguma exceção?
+                -qual a visibilidade do método?
+
+            */
     }
 }
