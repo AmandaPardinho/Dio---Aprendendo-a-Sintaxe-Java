ccabd2486350b0d957047e787410c37cd7a00ae9
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-26 22:53:10 -0300

    Comparing strings

--- operadores/src/operadores/OperadoresRelacionais.java
+++ operadores/src/operadores/OperadoresRelacionais.java
@@ -2,10 +2,17 @@ package operadores;
 
 public class OperadoresRelacionais {
     public static void main(String[] args) {
+        //comparando inteiros usando relacionais
         int numero1 = 1;
         int numero2 = 2;
 
         boolean simNao = numero1 == numero2;
         System.out.println(simNao);
+
+        //comparando strings usando relacionais
+        String nome1 = "Amanda";
+        String nome2 = "Amanda";
+
+        System.out.println(nome1 == nome2);
     }
 }
