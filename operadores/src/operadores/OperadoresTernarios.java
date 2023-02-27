07845168559fa991b789cc6ca815e25f51344acf
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-02-26 22:11:40 -0300

    New java class and part of its code

--- operadores/src/operadores/OperadoresTernarios.java
+++ operadores/src/operadores/OperadoresTernarios.java
@@ -1,2 +1,18 @@
-package operadores;public class OperadoresTernarios {
+package operadores;
+
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        String resultado = "";
+
+        if(a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado);
+
+
+    }
 }
