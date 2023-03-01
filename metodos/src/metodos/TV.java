fd15047301560680c5dd05f498fc0fb2521a2a81
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-01 19:53:54 -0300

    Beginning of the code with a new resolution idea

--- metodos/src/metodos/TV.java
+++ metodos/src/metodos/TV.java
@@ -1,2 +1,33 @@
-package metodos;public class TV {
+package metodos;
+
+import java.util.Scanner;
+
+public class TV {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int ligaDesliga;
+
+        System.out.println("""Selecione a opção:
+                1- Ligar a TV;
+                2- Desligar a TV;
+                3- Sair; 
+                """);
+
+        ligaDesliga = scanner.nextInt();
+
+        switch (ligaDesliga){
+            case 1:
+                break;
+            case 2;
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("Opção não disponível");
+                break;
+
+        }
+
+    }
 }
