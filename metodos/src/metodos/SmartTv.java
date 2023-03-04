66be2c47a6dbd802072c997c0bb9dcff4d6254e4
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-04 19:20:27 -0300

    Start of code for class SmartTv

--- metodos/src/metodos/SmartTv.java
+++ metodos/src/metodos/SmartTv.java
@@ -1,8 +1,23 @@
 package metodos;
 
-public class AssiteTv {
+public class SmartTv {
     STATUS statusTv;
+    String simNao;
 
+    public String getSimNao() {
+        return simNao;
+    }
 
+    public void setSimNao(String simNao) {
+        this.simNao = simNao;
+    }
+
+    public String assitirTv(String simNao){
+        if(simNao.equals("s") || simNao.equals("S")){
+            statusTv = STATUS.LIGAR;
+        }else{
+            statusTv = STATUS.DESLIGAR;
+        }
+    }
 
 }
