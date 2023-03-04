00d4e886ec13c0ae213c2ff76eeab7ec42b8755a
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-04 19:23:47 -0300

    Start of code for class Canal

--- metodos/src/metodos/Canal.java
+++ metodos/src/metodos/Canal.java
@@ -1,4 +1,22 @@
 package metodos;
 
 public class Canal {
+    private int canal;
+    private boolean mudaCanal;
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public boolean isMudaCanal() {
+        return mudaCanal;
+    }
+
+    public void setMudaCanal(boolean mudaCanal) {
+        this.mudaCanal = mudaCanal;
+    }
 }
