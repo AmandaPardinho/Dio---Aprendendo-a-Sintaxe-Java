5353338fe21a5a5c1a545c44aa02f7ddda21f645
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-04 19:19:34 -0300

    Start of code for class volume

--- metodos/src/metodos/Volume.java
+++ metodos/src/metodos/Volume.java
@@ -1,4 +1,31 @@
 package metodos;
 
 public class Volume {
+    private int volume;
+    private int aumentaVolume;
+    private int diminuiVolume;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getAumentaVolume() {
+        return aumentaVolume;
+    }
+
+    public void setAumentaVolume(int aumentaVolume) {
+        this.aumentaVolume = aumentaVolume;
+    }
+
+    public int getDiminuiVolume() {
+        return diminuiVolume;
+    }
+
+    public void setDiminuiVolume(int diminuiVolume) {
+        this.diminuiVolume = diminuiVolume;
+    }
 }
