81ef5975924f56cc22ed38612c910a97f0e43dd0
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-05 19:41:53 -0300

    Creating new variables and their methods and the method mudarCanal

--- metodos/src/metodos/Canal.java
+++ metodos/src/metodos/Canal.java
@@ -1,22 +1,69 @@
 package metodos;
 
 public class Canal {
-    private int canal;
-    private boolean mudaCanal;
+    private String canal;
+    private String mesmoCanal;
+    private String outroCanalMaior;
+    private String canalAcima;
+    private String canalAbaixo;
 
-    public int getCanal() {
+    public String getCanal() {
         return canal;
     }
 
-    public void setCanal(int canal) {
+    public void setCanal(String canal) {
         this.canal = canal;
     }
 
-    public boolean isMudaCanal() {
-        return mudaCanal;
+    public String getMesmoCanal() {
+        return mesmoCanal;
     }
 
-    public void setMudaCanal(boolean mudaCanal) {
-        this.mudaCanal = mudaCanal;
+    public void setMesmoCanal(String mesmoCanal) {
+        this.mesmoCanal = mesmoCanal;
+    }
+
+    public String getOutroCanalMaior() {
+        return outroCanalMaior;
+    }
+
+    public void setOutroCanalMaior(String outroCanalMaior) {
+        this.outroCanalMaior = outroCanalMaior;
+    }
+
+    public String getCanalAcima() {
+        return canalAcima;
+    }
+
+    public void setCanalAcima(String canalAcima) {
+        this.canalAcima = canalAcima;
+    }
+
+    public String getCanalAbaixo() {
+        return canalAbaixo;
+    }
+
+    public void setCanalAbaixo(String canalAbaixo) {
+        this.canalAbaixo = canalAbaixo;
+    }
+
+    public String mudarCanal(String mesmoCanal){
+
+        switch (mesmoCanal){
+            case "s":
+                return canal;
+            case "n":
+                if(outroCanalMaior.equals("s") || outroCanalMaior.equals("S")){
+                    canal = canalAcima;
+                    return canalAcima;
+                }else{
+                    canal = canalAbaixo;
+                    return canalAbaixo;
+                }
+            default:
+                System.out.println("Insira s/n");
+                break;
+        }
+        return canal;
     }
 }
