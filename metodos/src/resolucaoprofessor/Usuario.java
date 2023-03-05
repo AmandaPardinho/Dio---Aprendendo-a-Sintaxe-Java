c59e8014d256fd1360f140905430db435df0535c
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-05 20:18:44 -0300

    Implementation of the code used in the teacher's resolution

--- metodos/src/resolucaoprofessor/Usuario.java
+++ metodos/src/resolucaoprofessor/Usuario.java
@@ -1,2 +1,26 @@
-package resolucaoprofessor;public class Usuario {
+package resolucaoprofessor;
+
+public class Usuario {
+    public static void main(String[] args) {
+        //Resolução do exercício pelo professor
+        //Minha resolução está no pacote metodos
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("\nTV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("\nNovo Status => TV ligada? " + smartTv.ligada + "\n");
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume + "\n");
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+    }
 }
