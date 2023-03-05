8e023f0f1658f751453af1de3e7957cb5131f185
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-05 19:01:01 -0300

    Code to simulate change in volume

--- metodos/src/metodos/SmartTv.java
+++ metodos/src/metodos/SmartTv.java
@@ -6,5 +6,15 @@ public class SmartTv {
 
         tv.setSimNao("S");
         System.out.println(tv.assitirTv(tv.getSimNao()) + " a TV");
+
+        Volume volumeTV = new Volume();
+        volumeTV.setVolumeIdeal("n");
+        volumeTV.setVolume("15");
+        volumeTV.setVolumeMaior("s");
+        volumeTV.setAumentaVolume("25");
+        System.out.println(volumeTV.mudarVolume(volumeTV.getVolumeIdeal()));
+
+
+
     }
 }
