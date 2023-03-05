9fc0c973bd26498728ad59b64ab3874cd5baba5d
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-05 16:53:50 -0300

    Beginning of the exercise solution in the main class

--- metodos/src/metodos/SmartTv.java
+++ metodos/src/metodos/SmartTv.java
@@ -1,4 +1,10 @@
 package metodos;
 
 public class SmartTv {
+    public static void main(String[] args) {
+        LigaDesliga tv = new LigaDesliga();
+
+        tv.setSimNao("S");
+        System.out.println(tv.assitirTv(tv.getSimNao()) + " a TV");
+    }
 }
