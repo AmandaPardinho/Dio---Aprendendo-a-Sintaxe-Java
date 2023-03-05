c18fe2b887608f25ab73856297d2586adddbf7dc
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-05 20:19:08 -0300

    Implementation of the code used in the teacher's resolution

--- metodos/src/resolucaoprofessor/SmartTv.java
+++ metodos/src/resolucaoprofessor/SmartTv.java
@@ -1,2 +1,41 @@
-package resolucaoprofessor;public class SmartTv {
+package resolucaoprofessor;
+
+public class SmartTv {
+    //Resolução do exercício pelo professor
+        //Minha resolução está no pacote metodos
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
 }
