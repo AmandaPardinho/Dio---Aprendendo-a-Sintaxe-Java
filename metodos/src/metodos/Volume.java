d5e240b67ae5c653ef1a70524de16d115ffb65cb
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-05 18:59:30 -0300

    Implementation of new variables and the mudarVolume method

--- metodos/src/metodos/Volume.java
+++ metodos/src/metodos/Volume.java
@@ -1,31 +1,88 @@
 package metodos;
 
 public class Volume {
-    private int volume;
-    private int aumentaVolume;
-    private int diminuiVolume;
+    private String volume;
+    private String aumentaVolume;
+    private String diminuiVolume;
+    private String volumeIdeal;
+    private String volumeMaior;
+    private String volumeMenor;
+    private String volumeMantido;
 
-    public int getVolume() {
+    public String getVolume() {
         return volume;
     }
 
-    public void setVolume(int volume) {
+    public void setVolume(String volume) {
         this.volume = volume;
     }
 
-    public int getAumentaVolume() {
+    public String getAumentaVolume() {
         return aumentaVolume;
     }
 
-    public void setAumentaVolume(int aumentaVolume) {
+    public void setAumentaVolume(String aumentaVolume) {
         this.aumentaVolume = aumentaVolume;
     }
 
-    public int getDiminuiVolume() {
+    public String getDiminuiVolume() {
         return diminuiVolume;
     }
 
-    public void setDiminuiVolume(int diminuiVolume) {
+    public void setDiminuiVolume(String diminuiVolume) {
         this.diminuiVolume = diminuiVolume;
+    }
+
+    public String getVolumeIdeal() {
+        return volumeIdeal;
+    }
+
+    public void setVolumeIdeal(String volumeIdeal) {
+        this.volumeIdeal = volumeIdeal;
+    }
+
+    public String getVolumeMaior() {
+        return volumeMaior;
+    }
+
+    public void setVolumeMaior(String volumeMaior) {
+        this.volumeMaior = volumeMaior;
+    }
+
+    public String getVolumeMenor() {
+        return volumeMenor;
+    }
+
+    public void setVolumeMenor(String volumeMenor) {
+        this.volumeMenor = volumeMenor;
+    }
+
+    public String getVolumeMantido() {
+        return volumeMantido;
+    }
+
+    public void setVolumeMantido(String volumeMantido) {
+        this.volumeMantido = volumeMantido;
+    }
+
+    public String mudarVolume(String volumeIdeal){
+
+        switch(volumeIdeal){
+            case "s":
+                volumeMantido = volume;
+                break;
+            case "n":
+                if(volumeMaior.equals("s") || volumeMaior.equals("S")){
+                    volume = aumentaVolume;
+                    return aumentaVolume;
+                }else{
+                    volume = diminuiVolume;
+                    return diminuiVolume;
+                }
+            default:
+                System.out.println("Insira s/n");
+                break;
+        }
+        return volume;
     }
 }
