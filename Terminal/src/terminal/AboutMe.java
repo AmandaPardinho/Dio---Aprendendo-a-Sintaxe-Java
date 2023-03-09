222611da77e42ab651a97874c557c0d91ad7bafe
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-09 17:30:07 -0300

    New java file and its code

--- Terminal/src/terminal/AboutMe.java
+++ Terminal/src/terminal/AboutMe.java
@@ -1,2 +1,14 @@
-package terminal;public class AboutMe {
+package terminal;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Olá! Me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + "m");
+    }
 }
