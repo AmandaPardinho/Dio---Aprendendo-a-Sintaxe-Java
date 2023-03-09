04c5ef0384969ed775f72a76b4114bf7621e9d91
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-08 22:50:06 -0300

    Learning how to create documentation for Java code

--- documentacao/src/documentacao/Calculadora.java
+++ documentacao/src/documentacao/Calculadora.java
@@ -1,2 +1,24 @@
-package documentacao;public class Calculadora {
+package documentacao;
+
+/**
+ * <h1>Calculadora</h1>
+ * <br>
+ * <p>
+ *     A Calculadora realiza operações matemáticas entre números inteiros.
+ *     <b>Observação:</b> leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos
+ *     pela autora.
+ * </p>
+ * <br>
+ * @author AmandaPardinho
+ * @version 1.0
+ * @since 08/03/2023
+ */
+
+public class Calculadora {
+    /**
+     * <b>Este método é utilizado para somar dois números inteiros.</b>
+     * @param numeroUm => este é o primeiro parâmetro do método.
+     * @param numeroDois => este é o segundo parâmetro do método.
+     * @return int => o resultado deste método é a soma de dois números.
+     */
 }
