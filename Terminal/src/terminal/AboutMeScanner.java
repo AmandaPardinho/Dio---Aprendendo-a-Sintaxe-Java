2834973ba7710473036bd342edfb4542fcdb125b
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-10 22:16:34 -0300

    ;

--- Terminal/src/terminal/AboutMeScanner.java
+++ Terminal/src/terminal/AboutMeScanner.java
@@ -1,2 +1,30 @@
-package terminal;public class AboutMeScanner {
+package terminal;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMeScanner {
+    public static void main(String[] args) {
+        //Criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        //Imprimindo os dados obtidos
+        System.out.println("Olá! Me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " m");
+
+        scanner.close();
+    }
 }
